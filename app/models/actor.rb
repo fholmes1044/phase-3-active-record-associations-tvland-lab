639f862b927f36af633a2346469d1911d8c8ea4f
+ <+>
2023-03-12 17:03:05 -0400

    commit tree d07b21ce043aa9ccc74ae90ff2e511dfc8172664

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
+require "pry"
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name 
+    full_name = "#{self.first_name} #{self.last_name}"
+    full_name
+  end
+
+  def list_roles 
+   
+  role_map = self.characters.map do |character|
+    role = "#{character.name} - #{character.show.name}"
+   end 
+  end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,11 @@
+require "pry"
 class Network < ActiveRecord::Base
+  has_many :shows
+
+  def sorry 
+    sorry = "We're sorry about passing on John Mulaney's pilot"
+    sorry
+  end
+
   
 end
